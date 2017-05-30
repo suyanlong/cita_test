a17856ec5219602451fd21879eb96811b4a5baf4
suyanlong <592938410@qq.com>
2017-05-30 23:20:58 +0800

    引入boostrap库

---
+++ public/javascripts/browser.js
@@ -0,0 +1,11 @@
+/**
+ * Created by suyanlong on 2017/5/30.
+ */
+
+$(document).ready(
+    function () {
+        $(".btn").click(function () {
+            console.log(arguments);
+        });
+    }
+);
---
+++ routes/cita_test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by suyanlong on 2017/5/30.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET cita_test listing. */
+router.get('/', function (req, res, next) {
+    console.log("------------cita_test-------------");
+    console.log("----" + req.param("ip"));
+    console.log("---port--" + req.param("port"));
+
+
+
+    res.send('respond with a resource');
+});
+
+module.exports = router;
+
